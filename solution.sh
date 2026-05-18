73a99620de55993d882a5876b35a6f59a517e031
elinuraulia <auliaelly545@gmail.com>
2026-05-18 14:13:29 +0000

    Clean repository structure

--- solution.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p output
-# Hitung jumlah status
-awk -F 'NR>1 {print tolower($6)}' data.csv  | sort | uniq -c > output/status-count.txt
-# Filter gmail
-awk -F 'NR==1 || tolower($3) ~ /@gmail\.com$/' data.csv > output/gmail-customer.csv
-# Hapus duplicate email
-awk -F, 'NR==1 || !seen[tolower($3)]++' data.csv > output/no-duplicate.csv
-
